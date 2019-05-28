98ffbe5405d9586ebe57c82d6a7136f6fcdc8fcc
Marta Kocmidova <kocmidova.marta@seznam.cz>
2019-05-28 14:20:10 +0200

    Updates

---
+++ precache-manifest.0e823ce8ac9b38793490ffe0009e865a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "af593c95ce404838b064",
+    "url": "/static/css/main.fe214426.chunk.css"
+  },
+  {
+    "revision": "af593c95ce404838b064",
+    "url": "/static/js/main.af593c95.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a0d611ea05fc84f4cafb",
+    "url": "/static/css/2.f211c616.chunk.css"
+  },
+  {
+    "revision": "a0d611ea05fc84f4cafb",
+    "url": "/static/js/2.a0d611ea.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "0de3fd09aa27f8bf0209e0c9bc0963e7",
+    "url": "/index.html"
+  }
+];
